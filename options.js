164d55b61af3ef63da3252af783d569e0459b7c8
filipeisho <fifacioni@gmail.com>
2020-10-19 23:29:39 +0200

    language selection is working

---
+++ options.js
@@ -0,0 +1,49 @@
+
+function populateVoiceList() {
+  if(typeof speechSynthesis === 'undefined') {
+    return;
+  }
+
+  var voices = speechSynthesis.getVoices();
+
+  for(var i = 0; i < voices.length; i++) {
+    var option = document.createElement('option');
+    option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+    
+    if(voices[i].default) {
+      option.textContent += ' -- DEFAULT';
+    }
+
+    option.setAttribute('data-lang', voices[i].lang);
+    option.setAttribute('data-name', voices[i].name);
+    document.getElementById("voiceSelect").appendChild(option);
+  }
+}
+
+populateVoiceList();
+if (typeof speechSynthesis !== 'undefined' && speechSynthesis.onvoiceschanged !== undefined) {
+  speechSynthesis.onvoiceschanged = populateVoiceList;
+}
+function saveOptions(e) {
+  e.preventDefault();
+  browser.storage.sync.set({
+    voiceSelect: document.querySelector("#voiceSelect").value
+  });
+}
+
+function restoreOptions() {
+
+  function setCurrentChoice(result) {
+    document.querySelector("#voiceSelect").value = result.voiceSelect || "en-US";
+  }
+
+  function onError(error) {
+    console.log(`Error: ${error}`);
+  }
+
+  let getting = browser.storage.sync.get("voiceSelect");
+  getting.then(setCurrentChoice, onError);
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
--- pronounce.js
+++ pronounce.js
@@ -1,7 +1,29 @@
+
+function onError(error) {
+  console.log(`Error: ${error}`);
+}
+
+function onGot(item) {
+  let voice = "Jorge";
+  if (item.voiceSelect) {
+    voice = item.voiceSelect.split(" ")[0];
+  }
 document.ondblclick = function () {
    var sel = (document.selection && document.selection.createRange().text) ||
              (window.getSelection && window.getSelection().toString());
 
-let utterance = new SpeechSynthesisUtterance(sel);
-speechSynthesis.speak(utterance);
+  let utterance = new SpeechSynthesisUtterance(sel);
+  let voices = speechSynthesis.getVoices();
+  for(i = 0; i < voices.length ; i++) {
+    if(voices[i].name === voice) {
+      utterance.voice = voices[i];
+    }
+  }
+  speechSynthesis.speak(utterance);
 };
+
+}
+
+
+let getting = browser.storage.sync.get("voiceSelect");
+getting.then(onGot, onError);
