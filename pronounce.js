477cc93b8a471f73869ae378b2fb860aa3749bd9
filipeisho <fifacioni@gmail.com>
2020-10-20 13:21:24 +0200

    Selections of more than one word are not pronounced

--- pronounce.js
+++ pronounce.js
@@ -1,4 +1,3 @@
-
 function onError(error) {
   console.log(`Error: ${error}`);
 }
@@ -8,22 +7,29 @@ function onGot(item) {
   if (item.voiceSelect) {
     voice = item.voiceSelect.split(" ")[0];
   }
-document.ondblclick = function () {
-   var sel = (document.selection && document.selection.createRange().text) ||
-             (window.getSelection && window.getSelection().toString());
+  document.ondblclick = function () {
+    var sel =
+      (document.selection && document.selection.createRange().text) ||
+      (window.getSelection && window.getSelection().toString());
+    console.log(sel.split(" ").length);
+    const regex = /[^\s]+/gm;
+    const found = sel.match(regex);
+    if (found.length == 1) {
+      let utterance = new SpeechSynthesisUtterance(sel);
 
-  let utterance = new SpeechSynthesisUtterance(sel);
-  let voices = speechSynthesis.getVoices();
-  for(i = 0; i < voices.length ; i++) {
-    if(voices[i].name === voice) {
-      utterance.voice = voices[i];
+      let voices = speechSynthesis.getVoices();
+      for (i = 0; i < voices.length; i++) {
+        if (voices[i].name === voice) {
+          utterance.voice = voices[i];
+        }
+      }
+      speechSynthesis.speak(utterance);
     }
-  }
-  speechSynthesis.speak(utterance);
-};
-
+  };
 }
-
 
 let getting = browser.storage.sync.get("voiceSelect");
 getting.then(onGot, onError);
+
+
+
